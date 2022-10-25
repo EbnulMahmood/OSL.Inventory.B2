c58a14f529e7bb6c51f98c9b6b5078a74c25ed71
shovan <shovonmahmood67@gmail.com>
2022-10-25 18:07:22 +0600

    add base-entity

---
+++ OSL.Inventory.B2.Entity/BaseEntity/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using OSL.Inventory.B2.Entity.BaseEntity.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSL.Inventory.B2.Entity.BaseEntity
+{
+    public abstract class BaseEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+        public int Status { get; set; } = 1;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime ModifiedAt { get; set; }
+        public object CreatedBy { get; set; }
+        public object ModifiedBy { get; set; }
+
+        object IEntity.Id
+        {
+            get { return Id; }
+            set { }
+        }
+
+        object IEntity.CreatedBy
+        {
+            get { return CreatedBy; }
+            set { }
+        }
+
+        object IEntity.ModifiedBy
+        {
+            get { return ModifiedBy; }
+            set { }
+        }
+    }
+}
---
+++ OSL.Inventory.B2.Entity/BaseEntity/Interfaces/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSL.Inventory.B2.Entity.BaseEntity.Interfaces
+{
+    public interface IEntity
+    {
+        object Id { get; set; }
+        int Status { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime ModifiedAt { get; set; }
+        object CreatedBy { get; set; }
+        object ModifiedBy { get; set; }
+    }
+}
